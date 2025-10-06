b3d838d0ebdfa586871c9088b3e3d84bcfea47e7
Joaquin Lopez <159632612+elgatohumilde@users.noreply.github.com>
2025-10-05 21:31:13 -0500

    feat: add tags when publishing article and get tag recomendations

--- src/pages/articles/PublishPage.tsx
+++ src/pages/articles/PublishPage.tsx
@@ -1,13 +1,14 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { CreateArticleRequest } from "../../schemas/create-article-request";
 import { useClients } from "../../hooks/use-clients";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
 const PublishPage = () => {
   const navigate = useNavigate();
-  const { articlesClient } = useClients();
+  const { articlesClient, analystClient } = useClients();
   const [error, setError] = useState<string | null>(null);
 
   const form = useForm({
@@ -18,6 +19,12 @@ const PublishPage = () => {
       tags: [],
     },
   });
+
+  const { data: topTagsData } = useQuery({
+    queryKey: ["top-tags"],
+    queryFn: () => analystClient.findTopTags(),
+  })
+  const topTags = topTagsData?.body.map(res => res.tag);
 
   const onSubmit = async (data: CreateArticleRequest) => {
     try {
@@ -49,6 +56,61 @@ const PublishPage = () => {
           className="border border-neutral-950 rounded-md px-2 py-1 w-full min-h-50"
           {...form.register("content", { required: true })}
         />
+
+        <Controller
+          control={form.control}
+          name="tags"
+          render={({ field }) => (
+            <>
+              <div className="flex flex-wrap gap-2 mb-2">
+                {field.value.map((tag, i) => (
+                  <span
+                    key={i}
+                    className="bg-neutral-100 px-2 py-1 rounded-md flex items-center gap-1"
+                  >
+                    {tag}
+                    <button
+                      type="button"
+                      onClick={() => field.onChange(field.value.filter((_, j) => j != i))}
+                      className="">
+                      ×
+                    </button>
+                  </span>
+                ))}
+              </div>
+              <input
+                placeholder="Escribe una etiqueta y presiona Enter"
+                className="border border-neutral-950 rounded-md px-2 py-1 w-full"
+                onKeyDown={(e) => {
+                  if (e.key === "Enter" && e.currentTarget.value.trim()) {
+                    e.preventDefault();
+                    const newTag = e.currentTarget.value.trim();
+                    if (!field.value.includes(newTag))
+                      field.onChange([...field.value, newTag]);
+
+                    e.currentTarget.value = "";
+                  }
+                }}
+              />
+              {topTags && topTags.length > 0 && (
+                <div className="flex flex-wrap gap-2">
+                  {topTags.map((tag) => (
+                    <button
+                      key={tag}
+                      type="button"
+                      onClick={() => {
+                        if (!field.value.includes(tag)) field.onChange([...field.value, tag]);
+                      }}
+                      className="text-sm bg-neutral-100 hover:bg-neutral-300 rounded-md px-2 py-1">
+                      + {tag}
+                    </button>
+                  ))}
+                </div>
+              )}
+            </>
+          )}
+        />
+
         <div className="text-center">
           <button
             type="submit"
